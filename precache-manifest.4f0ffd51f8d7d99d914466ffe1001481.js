82a76b3338e072f8412266ee006e18cfa33d4c6d
Code Sandbox <hello@codesandbox.io>
2022-05-06 10:13:01 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4f0ffd51f8d7d99d914466ffe1001481.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "63ecad043c3093559a95",
+    "url": "https://warum48.github.io/csb-xmu9kz/static/css/main.2c63d8b7.chunk.css"
+  },
+  {
+    "revision": "63ecad043c3093559a95",
+    "url": "https://warum48.github.io/csb-xmu9kz/static/js/main.63ecad04.chunk.js"
+  },
+  {
+    "revision": "128f75c1288c5aa74e93",
+    "url": "https://warum48.github.io/csb-xmu9kz/static/css/1.be4bc3da.chunk.css"
+  },
+  {
+    "revision": "128f75c1288c5aa74e93",
+    "url": "https://warum48.github.io/csb-xmu9kz/static/js/1.128f75c1.chunk.js"
+  },
+  {
+    "revision": "a1a7b2e2d88e5ce6fa73",
+    "url": "https://warum48.github.io/csb-xmu9kz/static/js/runtime~main.a1a7b2e2.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "https://warum48.github.io/csb-xmu9kz/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "https://warum48.github.io/csb-xmu9kz/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "https://warum48.github.io/csb-xmu9kz/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "https://warum48.github.io/csb-xmu9kz/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "6af4a5624c76179a4f7dfc1b626021e5",
+    "url": "https://warum48.github.io/csb-xmu9kz/index.html"
+  }
+];
